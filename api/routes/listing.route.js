5972734d3fca9b6b5ef09f598801d06495cf5455
AsifAnsar-i <91591849+AsifAnsar-i@users.noreply.github.com>
2023-12-22 03:20:05 +0530

    complete update listing functionality

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { listingController ,deleteListingController,updateListingController} from "../controllers/listing.controller.js";
+import { listingController ,deleteListingController,updateListingController,getListingController} from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post("/create", verifyToken, listingController);
 router.delete("/delete/:id", verifyToken, deleteListingController);
 router.post("/update/:id", verifyToken, updateListingController);
+router.get("/get/:id", getListingController);
 
 export default router;
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -135,21 +135,23 @@ const Profile = () => {
     }
   };
 
-  const handleListingDelete=async(listingId)=>{
-       try {
-          const res = await fetch(`/api/listing/delete/${listingId}`,{
-            method:'DELETE'
-          })
-          const data = await res.json()
-          if(data.success===false){
-            console.warn(data.message)
-            return;
-          }
-          setUserListings((prev)=>prev.filter((listing)=>listing._id!==listingId));
-       } catch (error) {
-          console.warn(error.message)
-       }
-  }
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.warn(data.message);
+        return;
+      }
+      setUserListings((prev) =>
+        prev.filter((listing) => listing._id !== listingId)
+      );
+    } catch (error) {
+      console.warn(error.message);
+    }
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -237,24 +239,44 @@ const Profile = () => {
       <p className="text-red-700 mt-5">
         {showListingError ? "Error showing listings" : ""}
       </p>
-      {userListings && userListings.length>0 &&
-       <div className="flex flex-col gap-4">
-       <h1 className="text-center my-7 text-2xl font-semibold">Your Listings</h1>
-       { userListings.map((listing)=>(
-          <div className="border rounded-lg p-3 flex justify-between items-center gap-4" key={listing._id}>
-           <Link to={`/listing/${listing._id}`} >
-            <img src={listing.imageUrls[0]} alt="listing cover" className="h-16 w-16 object-contain"/>
-           </Link>
-           <Link className="flex-1 text-slate-700 font-semibold hover:underline truncate" to={`/listing/${listing._id}`}>
-            <p >{listing.name}</p>
-           </Link>
-           <div className="flex flex-col items-center">
-            <button onClick={()=>handleListingDelete(listing._id)} className="text-red-700 uppercase">Delete</button>
-            <button className="text-green-700 uppercase">Edit</button>
-           </div>
-          </div>
-        ))}</div>
-       }
+      {userListings && userListings.length > 0 && (
+        <div className="flex flex-col gap-4">
+          <h1 className="text-center my-7 text-2xl font-semibold">
+            Your Listings
+          </h1>
+          {userListings.map((listing) => (
+            <div
+              className="border rounded-lg p-3 flex justify-between items-center gap-4"
+              key={listing._id}
+            >
+              <Link to={`/listing/${listing._id}`}>
+                <img
+                  src={listing.imageUrls[0]}
+                  alt="listing cover"
+                  className="h-16 w-16 object-contain"
+                />
+              </Link>
+              <Link
+                className="flex-1 text-slate-700 font-semibold hover:underline truncate"
+                to={`/listing/${listing._id}`}
+              >
+                <p>{listing.name}</p>
+              </Link>
+              <div className="flex flex-col items-center">
+                <button
+                  onClick={() => handleListingDelete(listing._id)}
+                  className="text-red-700 uppercase"
+                >
+                  Delete
+                </button>
+                <Link to={`/update-listing/${listing._id}`}>
+                  <button className="text-green-700 uppercase">Edit</button>
+                </Link>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
