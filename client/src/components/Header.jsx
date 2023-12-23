dd67363cc896916c4c7a752c32162bd50647c908
AsifAnsar-i <91591849+AsifAnsar-i@users.noreply.github.com>
2023-12-23 17:59:25 +0530

    home functionality and about page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -40,7 +40,7 @@ const Header = () => {
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
           />
-          <FaSearch className="text-slate-600" />
+          <FaSearch onClick={handleSubmit}  className="text-slate-600 cursor-pointer" />
         </form>
         <ul className="flex gap-4">
           <Link to="/">
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
-const About = () => {
+export default function About() {
   return (
-    <div><>About</></div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Asif Estate</h1>
+      <p className='mb-4 text-slate-700'>Asif Estate is a leading real estate agency that specializes in helping clients buy, sell, and rent properties in the most desirable neighborhoods. Our team of experienced agents, including Asif, a full-stack developer, is dedicated to providing exceptional service and making the buying and selling process as smooth as possible.
+
+Our mission is to help our clients achieve their real estate goals by providing expert advice, personalized service, and a deep understanding of the local market. Whether you are looking to buy, sell, or rent a property, we are here to help you every step of the way.</p>
+      <p className='mb-4 text-slate-700'>
+      Asif, our full-stack developer, brings a unique perspective to our team, ensuring that we stay at the forefront of technology to enhance our services. With a wealth of experience and knowledge in both real estate and technology, we are committed to providing the highest level of service to our clients.
+      </p>
+      <p className='mb-4 text-slate-700'>
+Asif Estate is a leading real estate agency that specializes in helping clients buy, sell, and rent properties in the most desirable neighborhoods. Our team of experienced agents, including Asif, a full-stack developer, is dedicated to providing exceptional service and making the buying and selling process as smooth as possible.
+
+Our mission is to help our clients achieve their real estate goals by providing expert advice, personalized service, and a deep understanding of the local market. Whether you are looking to buy, sell, or rent a property, we are here to help you every step of the way.
+
+Asif, our full-stack developer, brings a unique perspective to our team, ensuring that we stay at the forefront of technology to enhance our services. With a wealth of experience and knowledge in both real estate and technology, we are committed to providing the highest level of service to our clients.
+
+We believe that buying or selling a property should be an exciting and rewarding experience, and we are dedicated to making that a reality for each and every one of our clients. If you're looking for a seamless real estate transaction and cutting-edge technological solutions, choose Asif Estate for all your real estate needs.</p>
+    </div>
   )
 }
-
-export default About
